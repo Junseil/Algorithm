39786f592ab570ee8122dc6826ea2bc7801f96d8
Seil Jun <ypd0709@naver.com>
2023-08-22 22:04:31 +0900

    23.08.22

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -3,11 +3,14 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-idx_data = deque([i for i in range(1, n+1)])
-val_data = deque(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+m = int(input())
+c = list(map(int, input().split()))
 
-while val_data:
-    print(idx_data.pop())
-    x = val_data.pop()
-    for i in range(x):
-        
+a_b = [b[i] for i in range(n) if a[i] == 0]
+x = c[::-1]
+x.extend(a_b)
+
+for _ in range(m):
+    print(x.pop(), end=' ')
