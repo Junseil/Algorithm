33f5534e8e11b501e0d896c1bcded7f4cb3c095b
Seil Jun <ypd0709@naver.com>
2023-08-23 13:51:20 +0900

    23.08.23

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -1,16 +1,8 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-m = int(input())
-c = list(map(int, input().split()))
+t = int(input())
 
-a_b = [b[i] for i in range(n) if a[i] == 0]
-x = c[::-1]
-x.extend(a_b)
-
-for _ in range(m):
-    print(x.pop(), end=' ')
+for _ in range(t):
+    a, b = map(int, input().split())
+    print((a**b)%10)
