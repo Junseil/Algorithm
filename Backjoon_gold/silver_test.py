da19fe6dc1ac8eaa96b9f7f78cc3a121bb72f0ff
Seil Jun <ypd0709@naver.com>
2023-08-24 18:18:01 +0900

    23.08.24

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -1,8 +1,13 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-t = int(input())
+n, m = map(int, input().split())
+min_set = 1000
+min_one = 1000
+for _ in range(m):
+    set_price, one_price = map(int, input().split())
+    min_set = min(min_set, set_price)
+    min_one = min(min_one, one_price)
 
-for _ in range(t):
-    a, b = map(int, input().split())
-    print((a**b)%10)
+print(min(min_one*n, min_set*(n//6)+min_one*(n%6), min_set*(n//6+1)))
