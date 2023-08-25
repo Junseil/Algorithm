50f0377c4b273cf4a71e5ffa13dd32291ed01710
Seil Jun <ypd0709@naver.com>
2023-08-25 17:37:14 +0900

    23.08.25

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -2,12 +2,27 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-min_set = 1000
-min_one = 1000
-for _ in range(m):
-    set_price, one_price = map(int, input().split())
-    min_set = min(min_set, set_price)
-    min_one = min(min_one, one_price)
+basket = set()
 
-print(min(min_one*n, min_set*(n//6)+min_one*(n%6), min_set*(n//6+1)))
+m = int(input())
+
+for _ in range(m):
+    s = input().rstrip().split()
+    if s[0] == 'add' and int(s[1]) not in basket:
+        basket.add(int(s[1]))
+    elif s[0] == 'remove' and int(s[1]) in basket:
+        basket.remove(int(s[1]))
+    elif s[0] == 'check':
+        if int(s[1]) in basket:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == 'toggle':
+        if int(s[1]) in basket:
+            basket.remove(int(s[1]))
+        else:
+            basket.add(int(s[1]))
+    elif s[0] == 'all':
+        basket = set([i for i in range(1, 21)])
+    elif s[0] == 'empty':
+        basket = set()
