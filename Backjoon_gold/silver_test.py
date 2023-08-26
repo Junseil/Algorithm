5cdf3ec804d196f558b44363bb9883c3c9242d75
Seil Jun <ypd0709@naver.com>
2023-08-26 18:11:39 +0900

    23.08.26

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -1,28 +1,14 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
-basket = set()
+n = int(input())
+data = [list(map(int, input().split())) for _ in range(n)]
 
-m = int(input())
-
-for _ in range(m):
-    s = input().rstrip().split()
-    if s[0] == 'add' and int(s[1]) not in basket:
-        basket.add(int(s[1]))
-    elif s[0] == 'remove' and int(s[1]) in basket:
-        basket.remove(int(s[1]))
-    elif s[0] == 'check':
-        if int(s[1]) in basket:
-            print(1)
-        else:
-            print(0)
-    elif s[0] == 'toggle':
-        if int(s[1]) in basket:
-            basket.remove(int(s[1]))
-        else:
-            basket.add(int(s[1]))
-    elif s[0] == 'all':
-        basket = set([i for i in range(1, 21)])
-    elif s[0] == 'empty':
-        basket = set()
+a = 0
+b = 0
+def sol(data, size):
+    global a, b
+    
+    for i in range(size):
+        for j in range(size):
+            data[i][j]
