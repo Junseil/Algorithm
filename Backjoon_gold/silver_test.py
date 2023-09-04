87bca37e17e63a9f13b4bfe97abffb14c551ff30
Seil Jun <ypd0709@naver.com>
2023-09-04 22:57:55 +0900

    23.09.04

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -1,42 +1,25 @@
 import sys
 from collections import deque
-
-sys.setrecursionlimit(3000)
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
+data = [[] for _ in range(n+1)]
 
-data = []
-
-for _ in range(n):
-    data.append(list(map(int, input().split())))
-
-for tmp_i in range(n):
-    for tmp_j in range(m):
-        if data[tmp_i][tmp_j] == 2:
-            i = tmp_i
-            j = tmp_j
-            
-res = [[0 for _ in range(m)] for _ in range(n)]
-diff = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-
-data[i][j] = -1
-bfs_q = deque()
-bfs_q.append([i, j])
-
-while bfs_q:
-    [x, y] = bfs_q.popleft()
+for _ in range(m):
+    x, y = map(int, input().split())
+    data[x].append(y)
+    data[y].append(x)
     
-    for dx, dy in diff:
-        nx, ny = x+dx, y+dy
-        
-        if nx in range(n) and ny in range(m) and data[nx][ny] == 1:
-            bfs_q.append([nx, ny])
-            data[nx][ny] = -1
-            res[nx][ny] = res[x][y] + 1
-            
-for i in range(n):
-    for j in range(m):
-        if data[i][j] == 1:
-            res[i][j] = -1
-        print(res[i][j], end=' ')
-    print()
+for x in range(n+1):
+    data[x].sort()
+    
+q = deque([])
+q.extend(data[1])
+visited = [0 for _ in range(n+1)]
+visited[x] = 1
+
+while q:
+    x = q.popleft()
+    if not visited[x]:
+        visited[x] = 1
+        visi
