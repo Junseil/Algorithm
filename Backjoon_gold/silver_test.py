d7493fe1ced1eec8c60d3c33107eb5d0dc91da81
Seil Jun <ypd0709@naver.com>
2023-09-05 21:18:37 +0900

    23.09.05

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -3,23 +3,36 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-data = [[] for _ in range(n+1)]
+data = [list(input().rstrip()) for _ in range(n)]
+visited = [[0 for _ in range(m)] for _ in range(n)]
+direction = [(+1, 0), (-1, 0), (0, -1), (0, +1)]
 
-for _ in range(m):
-    x, y = map(int, input().split())
-    data[x].append(y)
-    data[y].append(x)
-    
-for x in range(n+1):
-    data[x].sort()
-    
-q = deque([])
-q.extend(data[1])
-visited = [0 for _ in range(n+1)]
-visited[x] = 1
+for i in range(n):
+    for j in range(m):
+        if data[i][j] == 'I':
+            start_x = i
+            start_y = j
+            break
+
+cnt = 0
+q = deque()
+q.append((start_x, start_y))
 
 while q:
-    x = q.popleft()
-    if not visited[x]:
-        visited[x] = 1
-        visi
+    (x, y) = q.popleft()
+    print(x, y)
+    if not visited[x][y]:
+        visited[x][y] = 1
+        for (dx, dy) in direction:
+            nx = x+dx
+            ny = y+dy
+            if nx in range(n) and ny in range(m):
+                if data[nx][ny] == 'O':
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+                elif data[nx][ny] == 'P':
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+                    cnt += 1
+
+print(cnt)
