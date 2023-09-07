a5315118051407ef176e35f95f18e04c6b65ed8f
Seil Jun <ypd0709@naver.com>
2023-09-07 19:14:06 +0900

    23.09.07

--- Backjoon_gold/silver_test.py
+++ Backjoon_gold/silver_test.py
@@ -2,16 +2,29 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-t = int(input())
+n, m = map(int, input().split())
+data = [[] for _ in range(n+1)]
 
-def dis(data_three):
-    tmp = {'E':0, 'I':0, 'S':0, 'N':0, 'T':0, 'F':0, 'J':0, 'P':0}
-    for i in range(3):
-        for j in range(4):
-            tmp[data_three[i][j]] += 1
-    return tmp['E']*tmp['I']+tmp['S']*tmp['N']+tmp['T']*tmp['F']+tmp['J']*tmp['P']
+for _ in range(m):
+    x, y = map(int, input().split())
+    data[x].append(y)
+    data[y].append(x)
 
-for _ in range(t):
-    n = int(input())
-    data = list(map(str, input().split()))
+for i in range(n+1):
+    data[i].sort(reverse=True)
     
+def dfs(x):
+    stack = [x]
+    visited = [0 for _ in range(n+1)]
+    cnt = 1
+    while stack:
+        tmp_x = stack.pop()
+
+        if visited[tmp_x] == 0:
+            visited[tmp_x] = cnt
+            stack.extend(data[tmp_x])
+    return visited        
+        
+    
+for i in range(n):
+    print(dfs(i))
